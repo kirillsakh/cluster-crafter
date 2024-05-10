7216bd157a160a140dda934fa4a58bd5d5f3b24a
kirill.sachnov <kirill.sachnov@heureka.cz>
2024-05-10 11:25:10 +0200

    Add changes from CR for file handlers

--- tests/file_handlers/test_numpy_handler.py
+++ tests/file_handlers/test_numpy_handler.py
@@ -6,15 +6,16 @@ from src.file_handlers.numpy_handler import NumpyHandler
 
 
 def test_load(
-    numpy_handler: NumpyHandler, array_4_d: np.ndarray, numpy_file_path: str
+    numpy_handler: NumpyHandler, test_array: np.ndarray, numpy_file_path: str
 ) -> None:
     loaded_data = numpy_handler.load(numpy_file_path)
-    assert np.array_equal(loaded_data, array_4_d)
+    np.testing.assert_array_equal(loaded_data, test_array)
 
 
-def test_save(numpy_handler: NumpyHandler, tmpdir: Path) -> None:
-    data = np.array([1, 2, 3])
+def test_save(
+    numpy_handler: NumpyHandler, tmpdir: Path, test_array: np.ndarray
+) -> None:
     file_path = str(tmpdir / "test_data.npy")
-    numpy_handler.save(data, file_path)
+    numpy_handler.save(test_array, file_path)
     saved_data = np.load(file_path)
-    assert np.array_equal(saved_data, data)
+    np.testing.assert_array_equal(saved_data, test_array)
